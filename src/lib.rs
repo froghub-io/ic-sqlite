4cdd79878d302a0f0ac603a2e2b4089bf101b619
FroghubMan <froghub@163.com>
2022-10-23 16:21:23 +0800

    ic sqlite achieve

---
+++ src/lib.rs
@@ -0,0 +1,118 @@
+mod vfs;
+
+use std::collections::HashMap;
+use std::sync::{RwLock, Mutex, Arc};
+use once_cell::sync::Lazy;
+use lazy_static::lazy_static;
+use rusqlite::{Connection, OpenFlags};
+use sqlite_vfs::register;
+
+static STORAGE: Lazy<RwLock<HashMap<String, String>>> = Lazy::new(|| RwLock::new(HashMap::new()));
+
+lazy_static! {
+    pub static ref CONN: Arc<Mutex<Connection>> = {
+        register("vfs", vfs::PagesVfs::<4096>::default(), true).unwrap();
+        let conn = Connection::open_with_flags_and_vfs(
+            "main.db",
+            OpenFlags::SQLITE_OPEN_READ_WRITE
+                | OpenFlags::SQLITE_OPEN_CREATE
+                | OpenFlags::SQLITE_OPEN_NO_MUTEX,
+            "vfs",
+        ).unwrap();
+        conn.execute_batch(
+            r#"
+            PRAGMA page_size=4096;
+            "#,
+        ).unwrap();
+
+        return Arc::new(Mutex::new(conn));
+    };
+
+}
+
+pub fn get_storage() -> HashMap<String, String> {
+    return std::mem::take(&mut *STORAGE.write().unwrap());
+}
+
+pub fn set_storage(data: HashMap<String, String>) {
+    *STORAGE.write().unwrap() = data;
+}
+
+fn page_count() -> u32 {
+    return match STORAGE.read().unwrap().get("page_count") {
+        Some(v) => v.parse::<u32>().expect("page count get fail"),
+        None => 0
+    }
+}
+
+fn set_page_count(pc: u32) {
+    STORAGE.write().unwrap().insert("page_count".to_string(), pc.to_string());
+}
+
+fn get_page(ix: u32, ptr: &mut [u8]) {
+    match STORAGE.read().unwrap().get(&ix.to_string()) {
+        Some(v) => {
+            let buf:Vec<u8> = hex::decode(v).unwrap();
+            ptr.copy_from_slice(&buf);
+        },
+        None => {}
+    }
+}
+
+fn put_page(ix: u32, ptr: String) {
+    STORAGE.write().unwrap().insert(ix.to_string(), ptr);
+    if ix + 1 >= page_count() {
+        set_page_count(ix + 1);
+    }
+}
+
+fn del_page(ix: u32) {
+    if STORAGE.read().unwrap().contains_key(&ix.to_string()) {
+        STORAGE.write().unwrap().remove(&ix.to_string());
+        if ix + 1 >= page_count() {
+            set_page_count(ix);
+        }
+    }
+}
+
+fn conn_sleep(ms: u32) {
+    std::thread::sleep(std::time::Duration::from_secs(ms.into()));
+}
+
+fn put(ix: String, ptr: String) {
+    STORAGE.write().unwrap().insert(ix, ptr);
+}
+
+fn get(ix: String) -> Option<Vec<u8>> {
+    match STORAGE.read().unwrap().get(&ix) {
+        Some(v) => Some(hex::decode(v).unwrap()),
+        None => None
+    }
+}
+
+fn del(ix: String) {
+    if STORAGE.read().unwrap().contains_key(&ix) {
+        STORAGE.write().unwrap().remove(&ix);
+    }
+}
+
+fn exists(ix: String) -> bool {
+    STORAGE.read().unwrap().contains_key(&ix)
+}
+
+fn set_len(ix: String, size: u64) {
+    let data = get(ix.clone());
+    let mut buf = vec![0u8; size as usize];
+    if let Some(mut data) = data {
+        if size as usize > data.len() {
+            let len = data.len();
+            (0..size as usize - len).for_each(|_|{
+                data.push(0u8);
+            });
+            buf = data;
+        } else {
+            buf.copy_from_slice(&data[0..size as usize]);
+        }
+    }
+    put(ix.clone(), hex::encode(buf));
+}
---
+++ src/vfs.rs
@@ -0,0 +1,304 @@
+use std::fmt::Debug;
+use std::io::{self, ErrorKind};
+use std::sync::{Arc, Mutex};
+use std::time::{Duration, Instant};
+
+use sqlite_vfs::{LockKind, OpenKind, OpenOptions, Vfs};
+
+#[derive(Default)]
+pub struct PagesVfs<const PAGE_SIZE: usize> {
+    lock_state: Arc<Mutex<LockState>>,
+}
+
+#[derive(Debug, Default)]
+struct LockState {
+    read: usize,
+    write: Option<bool>,
+}
+
+pub struct Connection<const PAGE_SIZE: usize> {
+    db_name: String,
+    kind: OpenKind,
+    lock_state: Arc<Mutex<LockState>>,
+    lock: LockKind,
+}
+
+impl<const PAGE_SIZE: usize> Vfs for PagesVfs<PAGE_SIZE> {
+    type Handle = Connection<PAGE_SIZE>;
+
+    fn open(&self, db: &str, opts: OpenOptions) -> Result<Self::Handle, std::io::Error> {
+        Ok(Connection {
+            db_name: db.to_string(),
+            kind: opts.kind,
+            lock_state: self.lock_state.clone(),
+            lock: LockKind::None,
+        })
+    }
+
+    fn delete(&self, db: &str) -> Result<(), std::io::Error> {
+        crate::del(db.to_string());
+        Ok(())
+    }
+
+    fn exists(&self, db: &str) -> Result<bool, std::io::Error> {
+        Ok(db == "main.db" && Connection::<PAGE_SIZE>::page_count() > 0 || crate::exists(db.to_string()))
+    }
+
+    fn temporary_name(&self) -> String {
+        String::from("main.db")
+    }
+
+    fn random(&self, buffer: &mut [i8]) {
+        let mut rng = rusqlite::ffi::Rand::new();
+        rng.fill_i8(buffer);
+    }
+
+    fn sleep(&self, duration: Duration) -> Duration {
+        let now = Instant::now();
+        crate::conn_sleep((duration.as_millis() as u32).max(1));
+        now.elapsed()
+    }
+}
+
+impl<const PAGE_SIZE: usize> sqlite_vfs::DatabaseHandle for Connection<PAGE_SIZE> {
+    type WalIndex = sqlite_vfs::WalDisabled;
+
+    fn size(&self) -> Result<u64, io::Error> {
+        if self.kind != OpenKind::MainDb {
+            let data = crate::get(self.db_name.clone());
+            return if let Some(data) = data {
+                Ok(data.len() as u64)
+            } else {
+                Ok(0)
+            }
+        }
+        let size = Self::page_count() * PAGE_SIZE;
+        Ok(size as u64)
+    }
+
+    fn read_exact_at(&mut self, buf: &mut [u8], offset: u64) -> Result<(), io::Error> {
+        if self.kind != OpenKind::MainDb {
+            let data = crate::get(self.db_name.clone());
+            return if let Some(data) = data {
+                if data.len() > offset as usize + buf.len() {
+                    buf.copy_from_slice(&data[offset as usize..offset as usize + buf.len()]);
+                }
+                Ok(())
+            } else {
+                Err(io::Error::new(
+                    ErrorKind::Other,
+                    format!(
+                        "unexpected write size {}; expected {}",
+                        buf.len(),
+                        0
+                    ),
+                ))
+            }
+        }
+        let index = offset as usize / PAGE_SIZE;
+        let offset = offset as usize % PAGE_SIZE;
+
+        let data = Self::get_page(index as u32);
+        if data.len() < buf.len() + offset {
+            return Err(ErrorKind::UnexpectedEof.into());
+        }
+
+        buf.copy_from_slice(&data[offset..offset + buf.len()]);
+
+        Ok(())
+    }
+
+    fn write_all_at(&mut self, buf: &[u8], offset: u64) -> Result<(), io::Error> {
+        if self.kind != OpenKind::MainDb {
+            crate::put(self.db_name.clone(), hex::encode(buf));
+            return Ok(())
+        }
+        if offset as usize % PAGE_SIZE > 0 {
+            return Err(io::Error::new(
+                ErrorKind::Other,
+                "unexpected write across page boundaries",
+            ));
+        }
+
+        let index = offset as usize / PAGE_SIZE;
+        let page = buf.try_into().map_err(|_| {
+            io::Error::new(
+                ErrorKind::Other,
+                format!(
+                    "unexpected write size {}; expected {}",
+                    buf.len(),
+                    PAGE_SIZE
+                ),
+            )
+        })?;
+        Self::put_page(index as u32, &page);
+
+        Ok(())
+    }
+
+    fn sync(&mut self, _data_only: bool) -> Result<(), io::Error> {
+        // Everything is directly written to storage, so no extra steps necessary to sync.
+        Ok(())
+    }
+
+    fn set_len(&mut self, size: u64) -> Result<(), io::Error> {
+        if self.kind != OpenKind::MainDb {
+            crate::set_len(self.db_name.clone(), size);
+            return Ok(())
+        }
+        let mut page_count = size as usize / PAGE_SIZE;
+        if size as usize % PAGE_SIZE > 0 {
+            page_count += 1;
+        }
+
+        let current_page_count = Self::page_count();
+        if page_count > 0 && page_count < current_page_count {
+            for i in (page_count..current_page_count).into_iter().rev() {
+                Self::del_page(i as u32);
+            }
+        }
+
+        Ok(())
+    }
+
+    fn lock(&mut self, lock: LockKind) -> Result<bool, io::Error> {
+        let ok = Self::lock(self, lock);
+        Ok(ok)
+    }
+
+    fn reserved(&mut self) -> Result<bool, io::Error> {
+        Ok(Self::reserved(self))
+    }
+
+    fn current_lock(&self) -> Result<LockKind, io::Error> {
+        Ok(self.lock)
+    }
+
+    fn set_chunk_size(&self, chunk_size: usize) -> Result<(), io::Error> {
+        if self.kind != OpenKind::MainDb {
+            return Ok(())
+        }
+        if chunk_size != PAGE_SIZE {
+            Err(io::Error::new(
+                ErrorKind::Other,
+                "changing chunk size is not allowed",
+            ))
+        } else {
+            Ok(())
+        }
+    }
+
+    fn wal_index(&self, _readonly: bool) -> Result<Self::WalIndex, io::Error> {
+        Ok(sqlite_vfs::WalDisabled::default())
+    }
+}
+
+impl<const PAGE_SIZE: usize> Connection<PAGE_SIZE> {
+    fn get_page(ix: u32) -> [u8; PAGE_SIZE] {
+        let mut data = [0u8; PAGE_SIZE];
+        crate::get_page(ix, &mut data);
+        data
+    }
+
+    fn put_page(ix: u32, data: &[u8; PAGE_SIZE]) {
+        crate::put_page(ix, hex::encode(data));
+    }
+
+    fn del_page(ix: u32) {
+        crate::del_page(ix);
+    }
+
+    fn page_count() -> usize {
+        crate::page_count() as usize
+    }
+
+    fn lock(&mut self, to: LockKind) -> bool {
+        if self.lock == to {
+            return true;
+        }
+
+        let mut lock_state = self.lock_state.lock().unwrap();
+
+        // The following locking implementation is probably not sound (wouldn't be surprised if it
+        // potentially dead-locks), but suffice for the experiment.
+
+        match to {
+            LockKind::None => {
+                if self.lock == LockKind::Shared {
+                    lock_state.read -= 1;
+                } else if self.lock > LockKind::Shared {
+                    lock_state.write = None;
+                }
+                self.lock = LockKind::None;
+                true
+            }
+
+            LockKind::Shared => {
+                if lock_state.write == Some(true) && self.lock <= LockKind::Shared {
+                    return false;
+                }
+
+                lock_state.read += 1;
+                if self.lock > LockKind::Shared {
+                    lock_state.write = None;
+                }
+                self.lock = LockKind::Shared;
+                true
+            }
+
+            LockKind::Reserved => {
+                if lock_state.write.is_some() || self.lock != LockKind::Shared {
+                    return false;
+                }
+
+                if self.lock == LockKind::Shared {
+                    lock_state.read -= 1;
+                }
+                lock_state.write = Some(false);
+                self.lock = LockKind::Reserved;
+                true
+            }
+
+            LockKind::Pending => {
+                // cannot be requested directly
+                false
+            }
+
+            LockKind::Exclusive => {
+                if lock_state.write.is_some() && self.lock <= LockKind::Shared {
+                    return false;
+                }
+
+                if self.lock == LockKind::Shared {
+                    lock_state.read -= 1;
+                }
+
+                lock_state.write = Some(true);
+                if lock_state.read == 0 {
+                    self.lock = LockKind::Exclusive;
+                    true
+                } else {
+                    self.lock = LockKind::Pending;
+                    false
+                }
+            }
+        }
+    }
+
+    fn reserved(&self) -> bool {
+        if self.lock > LockKind::Shared {
+            return true;
+        }
+
+        let lock_state = self.lock_state.lock().unwrap();
+        lock_state.write.is_some()
+    }
+}
+
+impl<const PAGE_SIZE: usize> Drop for Connection<PAGE_SIZE> {
+    fn drop(&mut self) {
+        if self.lock != LockKind::None {
+            self.lock(LockKind::None);
+        }
+    }
+}
